0962d47fad878fd709746b90579a4c4bb344ae1f
Juan Marcos Bellini <juanmbellini@gmail.com>
2016-09-24 18:04:41 -0300

    Exercise 1

---
+++ ej1.m
@@ -0,0 +1,43 @@
+function algunaFunction(N, NZR)
+
+	if (floor(N) != N)
+		printf('Dimension N must be integer\n');
+		return;
+	end
+	if (floor(NZR) != NZR)
+		printf('NZR value must be integer\n');
+		return;
+	end
+	if (N <= 0)
+		printf('Dimension N must be greater than 0\n');
+		return;
+	end
+	if (NZR < 0)
+		printf('NZR value must be at least 0\n');
+		return;
+	end
+	if N < NZR
+		printf('Dimension N cannot be less than NZR value\n');
+		return;
+	end
+
+	A = zeros(N); 	% A holds the future NZR Matrix
+
+	for columnIndex = 1 : N
+		for i = 1 : NZR
+			do
+				rowIndex = ceil(rand() * N);		% Gets a random row to assign a number to it
+			until (A(rowIndex, columnIndex) == 0)	% Repeat if the given row already holds a number
+			do 
+				A(rowIndex, columnIndex) = rand() * 2 - 1; 		% Gets a random number between -1 and 1
+			until (A(rowIndex, columnIndex) != 0)				% Checks if the given number is zero;
+		end
+	end
+	A
+end
+
+
+function debug(string) 
+	printf(string);
+	fflush(stdout);
+end
