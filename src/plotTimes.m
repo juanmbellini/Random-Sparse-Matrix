16dde4f71b09bf415c12cf160bf57620a8095a78
Luis Marzoratti <luis@localhost.localdomain>
2016-09-29 23:30:54 -0300

    separated benchmark and plot, added legend

---
+++ src/plotTimes.m
@@ -0,0 +1,9 @@
+function plotTimes(times)
+  plot(times(:,1),times(:,2), 'linewidth', 3, times(:,1),times(:,3), 'linewidth', 3);
+  set(gca, 'linewidth', 2, 'fontsize', 12);
+  title('Benchmark de descomposiciones para el cálculo de autovalores');
+  xlabel('Tamaño de la matriz (NxN)');
+  ylabel('Tiempo (s)');
+  [legh,objh,outh,outm] = legend({"Modified Gram Shmit","Givens Rotations"});
+  set(objh,'linewidth',2);
+end
--- src/test.m
+++ src/test.m
@@ -20,11 +20,3 @@ function times = test(test_sizes,NZR,error, repetitions)
   times(:, 2:end) = times(:, 2:end) ./ repetitions;
   plotTimes(times);
 end
-
-function plotTimes(times)
-  plot(times(:,1),times(:,2), 'linewidth', 3, times(:,1),times(:,3), 'linewidth', 3);
-  set(gca, 'linewidth', 2, 'fontsize', 12);
-  title('Benchmark de descomposiciones para el cálculo de autovalores');
-  xlabel('Tamaño de la matriz (NxN)');
-  ylabel('Tiempo (s)');
-end
